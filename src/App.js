4bc01f64c65bb29fbcb159a0a97b36d1e902f0dc
Mahlet Tiruneh <mtiruneh@uw.edu>
2021-03-17 22:08:50 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        
+        <h1>MAHLET TIRUNEH <p ><br/> [ma•ha•let]</p> </h1>
+        
+        <div className="nav">
+          <div className="work"> <a href="#work">work</a> </div>
+          <div className="about"> <a href="#about">about</a> </div>
+          <div className="resume"> <a href="#resume">resume</a> </div>
+        </div>
+
       </header>
+
+      <div className="main">
+        <div className="lpcontainer">
+          <div className="latest-project"></div>
+        </div>
+
+          <div className="projects1">
+            <div className="project1"></div>
+            <div className="project2"></div>
+          </div>
+
+          <div className="projects2">
+            <div className="project3"></div>
+            <div className="project4"></div>
+          </div>
+        </div>
+
     </div>
-  );
-}
+  )}
 
 export default App;
